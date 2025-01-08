292a93df90d80b7c3337fc18cf4525fe8b887466
shoaib <shoaibhauqe7@gmail.com>
2025-01-08 17:59:55 +0600

    added for wait of

--- api/async_await.js
+++ api/async_await.js
@@ -1,3 +1,7 @@
+/* Asynchronous iteration in JavaScript allows you to handle asynchronous data sources in a clean and structured way. 
+The for await...of loop is a modern tool designed for iterating over data from asynchronous sources, 
+such as Promises, async generators, or any object implementing the AsyncIterable protocol. */
+
 const BASEURL = "https://cat-fact.herokuapp.com";
 
 const getData = async () => {
--- asynchronous/for_await_of.js
+++ asynchronous/for_await_of.js
@@ -1,3 +1,7 @@
+/* Asynchronous iteration in JavaScript allows you to handle asynchronous data sources in a clean and structured way. 
+The for await...of loop is a modern tool designed for iterating over data from asynchronous sources, 
+such as Promises, async generators, or any object implementing the AsyncIterable protocol. */
+
 const URLS = [
     "https://jsonplaceholder.typicode.com/posts/1",
     "https://jsonplaceholder.typicode.com/posts/2",
@@ -9,5 +13,29 @@ const URLS = [
         let res = await fetch(url);
         let data = await res.json();
         console.log(data.title);
+    }
+})();
+
+const PROMISES = [
+    new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("1st");
+        }, 2000);
+    }),
+    new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("2nd");
+        }, 5000);
+    }),
+    new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("3rd");
+        }, 10000);
+    })
+];
+
+(async () => {
+    for await (let res of PROMISES) {
+        console.log(res);
     }
 })();
